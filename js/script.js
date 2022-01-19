249e8fac9e64900924f55249a569ccf3b756eccb
artisyn <artioms1@gmail.com>
2022-01-19 23:06:44 +0200

    added a lot of Javascript

--- js/script.js
+++ js/script.js
@@ -4,19 +4,56 @@ const displayWrong = document.querySelector('.wrong');
 const displayTime = document.querySelector('.time');
 const displayQuestionNum = document.querySelector('.questionNum');
 const question = document.querySelector('.question');
-
 const answer1 = document.querySelector('.answer1');
 const answer2 = document.querySelector('.answer2');
 const answer3 = document.querySelector('.answer3');
 const answer4 = document.querySelector('.answer4');
 const allAnswers = document.querySelectorAll('.answer');
-
 const btnReplay = document.querySelector('.btn__replay');
-const btnSend = document.querySelector('.btn__send');
+const btnNext = document.querySelector('.btn__next');
 let index = 0;
+let correctAnswersNum = 0;
+let wrongAnswersNum = 0;
+//timer elements
+let timer;
+let sec = 0;
+// functions
+
+//starting timer
+timer = setInterval(function () {
+  displayTime.innerHTML =
+    `⏰ ` +
+    `${parseInt(sec / 60, 10)}`.padStart(2, 0) +
+    `:` +
+    `${++sec % 60}`.padStart(2, 0);
+}, 1000);
+// clearing timer
+const clearTimer = function () {
+  clearInterval(timer);
+  sec = 0;
+};
 // helper function for getting randon number;
 const randomInt = (min, max) =>
   Math.floor(Math.random() * (max - min) + 1) + min;
+
+//function for clearing unnecessary classlists
+const clearClassNames = function () {
+  allAnswers.forEach((el) => {
+    if (el.classList.contains('correctAnswer'))
+      el.classList.remove('correctAnswer');
+
+    if (el.classList.contains('wrongAnswer'))
+      el.classList.remove('wrongAnswer');
+  });
+};
+
+const clearAll = function () {
+  index = correctAnswersNum = wrongAnswersNum = 0;
+  questionsApi();
+  clearTimer();
+  clearClassNames();
+};
+
 // implementing array shuffling
 const shuffleArray = function (arr) {
   const copyArr = [...arr];
@@ -30,32 +67,37 @@ const shuffleArray = function (arr) {
   return shuffledArr;
 };
 
+//loading questions for user
+const loadQuestions = function (i, questionsArray) {
+  let answersArr = [
+    questionsArray[i].correct_answer,
+    questionsArray[i].incorrect_answers[0],
+    questionsArray[i].incorrect_answers[1],
+    questionsArray[i].incorrect_answers[2],
+  ];
+  // shuffling the order of questions
+  answersArr = shuffleArray(answersArr);
+  // getting question number
+  displayQuestionNum.innerHTML = `Question ${i + 1}/10`;
+  // getting question  name
+  question.innerHTML = `${questionsArray[index].question}`;
+  //getting question options
+  answer1.innerHTML = answersArr[0];
+  answer2.innerHTML = answersArr[1];
+  answer3.innerHTML = answersArr[2];
+  answer4.innerHTML = answersArr[3];
+};
+
+// main funtion
 const questionsApi = async function () {
   const promise = await fetch(
     'https://opentdb.com/api.php?amount=10&category=31&difficulty=easy&type=multiple'
   );
   const result = await promise.json();
   const questionsArray = result.results;
-  console.log(questionsArray);
-  //creating answers array
-  let answersArr = [
-    questionsArray[index].correct_answer,
-    questionsArray[index].incorrect_answers[0],
-    questionsArray[index].incorrect_answers[1],
-    questionsArray[index].incorrect_answers[2],
-  ];
-  answersArr = shuffleArray(answersArr);
-  // getting question number
-  displayQuestionNum.innerHTML = `Question ${index + 1}/10`;
 
-  // getting question  name
-  question.innerHTML = `${questionsArray[index].question}`;
-
-  //getting question options
-  answer1.innerHTML = answersArr[0];
-  answer2.innerHTML = answersArr[1];
-  answer3.innerHTML = answersArr[2];
-  answer4.innerHTML = answersArr[3];
+  //loading questions
+  loadQuestions(index, questionsArray);
 
   console.log(allAnswers);
   allAnswers.forEach((el) => {
@@ -64,18 +106,36 @@ const questionsApi = async function () {
         el.lastElementChild.textContent === questionsArray[index].correct_answer
       ) {
         el.classList.add('correctAnswer');
+        ++correctAnswersNum;
+        displayCorrect.innerHTML = `✔ ${correctAnswersNum} correct`;
+        // game win func
       }
+
       if (
         el.lastElementChild.textContent !== questionsArray[index].correct_answer
       ) {
         el.classList.add('wrongAnswer');
+        ++wrongAnswersNum;
+        displayWrong.innerHTML = `❌ ${wrongAnswersNum} wrong`;
+        // find the right answer and show it
+        allAnswers.forEach((el) => {
+          if (
+            el.lastElementChild.textContent ===
+            questionsArray[index].correct_answer
+          )
+            el.classList.add('correctAnswer');
+        });
+        // game win func
       }
       console.log(el.lastElementChild.textContent);
     });
   });
 
-  btnSend.addEventListener('click', function () {
-    index = index++;
+  btnNext.addEventListener('click', function () {
+    clearClassNames();
+    index = index + 1;
+    console.log(index);
+    loadQuestions(index, questionsArray);
   });
 };
 
