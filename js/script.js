aa95be1e17a6ee0d91130b21ecbe4ab8343745df
artisyn <artioms1@gmail.com>
2022-01-19 15:50:46 +0200

    finished most of the css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
 'use strict';
+const displayCorrect = document.querySelector('.correct');
+const displayWrong = document.querySelector('.wrong');
+const displayTime = document.querySelector('.time');
+const displayQuestionNum = document.querySelector('.questionNum');
+const question = document.querySelector('.question');
+
+const answer1 = document.querySelector('.answer1');
+
+const btnReplay = document.querySelector('.btn__replay');
+const btnSend = document.querySelector('.btn__send');
